e32f2cafcce2bcd9c5094b750bb81ff1af89ce07
Ed Caspersen <ed.caspersen@gmail.com>
2020-08-28 14:48:45 -0700

    Updated documentation

--- invert_naray.py
+++ invert_naray.py
@@ -1,18 +1,19 @@
 """
-Given an N-ary group, take the values an invert them.
+Given an N-ary group, take the values and invert them left to right.
 Since the group (graph, tree, etc) can be arbitrary,
 like the example below, it can be effectively represented
 in a flat list like a binary tree example
 
-        0
-      / | \
-     /  |  \
-    1   2   3
-   /|\       \
-  / | \      /\
- 6  7  8    9 10
-              /\
-             11 12
+            0
+          / | \
+         /  |  \
+        1   2   3
+       /|\       \
+      / | \      /\
+     6  7  8    9 10
+                  /\
+                 /  \
+                11  12
 
 The aforementioned graph is expressed in nested tuples.
 Any index that has it's own children will be wrapped
@@ -21,6 +22,17 @@ the second is reserved for the children
 
 Here is an example graph represented in tuples
 (0, ((1, (6, 7, 8)), 2, (3, (9, (10, (11, 12))))))
+
+The resulting graph would then look like
+            0
+          / | \
+         /  |  \
+        3   2   1
+       /\      /|\
+      9 10    / | \
+        /\   8  7  6
+       /  \ 
+      11  12
 """
 
 
